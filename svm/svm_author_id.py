de643825ce3504a19449d413056e2f821ecb2de8
Aditi Goyal <31741158+Adiiigo@users.noreply.github.com>
2018-07-14 22:59:59 +0530

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -43,5 +43,8 @@ print 'Test Time = ' , round(time()-t1,3) , 's'
 acc = accuracy_score(labels_test,labels_pred)
 print 'Accuracy = ',acc
 #########################################################
+#to Decrease the time 
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 
 
