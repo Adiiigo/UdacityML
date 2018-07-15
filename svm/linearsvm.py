96930d6c4cb0f5b83996bc7d61097eaa0beeb54d
Aditi Goyal <31741158+Adiiigo@users.noreply.github.com>
2018-07-15 22:49:30 +0530

    Update linearsvm.py

--- svm/linearsvm.py
+++ svm/linearsvm.py
@@ -4,15 +4,24 @@ from matplotlib import style
 style.use("ggplot")
 from sklearn import svm
 
+#Giving hardcoded value of the features
 features_train = np.array([[1,2],[5,8],[1.5,1.8],[8,8],[1,0.6],[9,11]])
+
+#Deciding in which category they belong
 labels_train = [0,1,0,1,0,1]
+
+#Features to test the model
 feature_test = [(0.58,0.76)]
 
+#Using the support vector classifier as model
 clf = svm.SVC(kernel = "linear")
+#Training the model
 clf.fit(features_train , labels_train)
-
+#Predicting the value of the model
 print (clf.predict(feature_test))
 
+
+##Plotting the Line graph
 w = clf.coef_[0]
 print(w)
 
